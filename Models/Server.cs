7a64682ab21513b95fd4373dc3680e29d7b9ff28
alexandrudami <alexandrudami@gmail.com>
2025-02-20 16:59:41 +0200

    .

---
+++ Models/Server.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorSVM.Models
+{
+    public class Server
+    {
+        public Server() 
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 2);
+            IsOnline = randomNumber ==0? false : true;
+
+        }
+
+        public int ServerID { get; set; }
+        public bool IsOnline { get; set; }
+        public string? Name { get; set; }
+        public string? City { get; set; }
+    }
+}
